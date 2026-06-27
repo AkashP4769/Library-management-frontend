554093278220ea048ef22baa38d196da8a9b0420
Deepak <deepaks11a7@gmail.com>
2026-06-27 16:31:28 +0530

    feat: Add theme configs and path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,16 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
-})
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@images": path.resolve(__dirname, "./src/assets/images"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@tests": path.resolve(__dirname, "./src/tests"),
+      "@api-services": path.resolve(__dirname, "./src/api-services"),
+    },
+  },
+});
