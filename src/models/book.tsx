9496aeee6d9d08755e79af255c5cfcfac363f0c9
Akash <awesomeakash47@gmail.com>
2026-06-28 15:42:40 +0530

    fix: ui of home and catalog page

--- src/models/book.tsx
+++ src/models/book.tsx
@@ -9,6 +9,7 @@ export default interface Book {
     language: string;
     description: string;
     image: string;
+    rating: number;
     createdAt: string;
     updatedAt: string;
 }
@@ -23,6 +24,7 @@ export const books = [
         language: "English",
         description: "A classic American novel set in the Jazz Age.",
         image: "https://images.unsplash.com/photo-1554080353-a576cf803bda?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHBob3RvZ3JhcGh5fGVufDB8fDB8fHww",
+        rating: 4.5,
         createdAt: "2023-01-01T00:00:00Z",
         updatedAt: "2023-01-01T00:00:00Z"
     },
@@ -35,6 +37,7 @@ export const books = [
         language: "English",
         description: "A gripping tale of racial injustice and childhood innocence.",
         image: "https://images.unsplash.com/photo-1601134991665-a020399422e3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHBob3RvfGVufDB8fDB8fHww",
+        rating: 4.8,
         createdAt: "2023-01-01T00:00:00Z",
         updatedAt: "2023-01-01T00:00:00Z"
     },
@@ -47,6 +50,7 @@ export const books = [
         language: "English",
         description: "A dystopian social science fiction novel and cautionary tale.",
         image: "https://images.unsplash.com/photo-1531804055935-76f44d7c3621?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cGhvdG98ZW58MHx8MHx8fDA%3D",
+        rating: 4.9,
         createdAt: "2023-01-01T00:00:00Z",
         updatedAt: "2023-01-01T00:00:00Z"
     },
@@ -59,6 +63,7 @@ export const books = [
         language: "English",
         description: "A controversial novel that has been the subject of censorship debates.",
         image: "https://images.unsplash.com/photo-1457089328109-e5d9bd499191?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHBob3RvfGVufDB8fDB8fHww",
+        rating: 4.2,
         createdAt: "2023-01-01T00:00:00Z",
         updatedAt: "2023-01-01T00:00:00Z"
     }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,10 @@ import type Book from "@/models/book";
 import { books } from "@/models/book";
 import { useState } from "react";
 import './Home.css'
+import BookCard, { SmallBookCard } from "@/Components/BookCard";
 
 export default function HomePage() {
-    const [myBooks, setMyBooks] = useState<Book[]>(books);
+    const [myBooks, setMyBooks] = useState<Book[]>([...books, ...books]);
 
     return <div className="home-page">
         <div className="flex justify-between items-center">
@@ -12,14 +13,9 @@ export default function HomePage() {
             <p className="text-primary font-bold hover:underline cursor-pointer">VIEW ALL</p>
         </div>
         <div className="grid grid-cols-5 gap-6">
-            {myBooks.length === 0 ? <p className="text-bold text-primary text-xl">No books found</p> : myBooks.map((book) => (
-                <div key={book.id} className="flex flex-col items-start justify-center px-4 py-4">
-                    <img src={book.image} alt={book.title} className="w-full h-full object-cover rounded-2xl" />
-                    <div className="w-full">
-                        <h3 className="text-lg font-bold mt-2 text-ellipsis">{book.title}</h3>
-                        <p className="text-tertiary">{book.author}</p>
-                    </div>
-                </div>
+            {myBooks.length === 0 ? <p className="text-bold text-primary text-xl">No books found</p> : 
+            myBooks.map((book) => (
+                <BookCard key={book.id} {...book} />
             ))}
         </div>
 
@@ -27,15 +23,9 @@ export default function HomePage() {
             <h2 className="text-2xl font-bold">Most Popular this week</h2>
             <p className="text-primary font-bold hover:underline cursor-pointer">VIEW ALL</p>
         </div>
-        <div className="flex flex-wrap gap-6">
+        <div className="flex flex-wrap gap-x-6 gap-y-4 mt-4">
             {myBooks.length === 0 ? <p className="text-bold text-primary text-xl">No books found</p> : myBooks.map((book) => (
-                <div key={book.id} className="flex items-center gap-4 justify-center h-60 bg-white px-8 py-4 w-70 border-2 border-neutral-200 rounded-2xl">
-                    <img src={book.image} alt={book.title} className="w-full h-50 object-cover rounded-2xl" />
-                    <div className="w-60 px-4">
-                        <h3 className="text-lg font-bold mt-2 text-ellipsis">{book.title}</h3>
-                        <p className="text-tertiary">{book.author}</p>
-                    </div>
-                </div>
+                <SmallBookCard key={book.id} {...book} />
             ))}
         </div>
     </div>
