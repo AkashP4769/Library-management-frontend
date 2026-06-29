c8c99cd0d71b4268c8bc3b77271d86e239b3e60f
Aksa <aksageorge2703@gmail.com>
2026-06-29 11:59:47 +0530

    fix: layout ui

--- src/pages/employee/Layout.tsx
+++ src/pages/employee/Layout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import './Layout.css'
+import "./Layout.css";
 
 import { Link } from "react-router";
 import DashboardIcon from "@assets/icons/sidebar_dashboard.svg";
@@ -11,68 +11,98 @@ import InventoryIcon from "@assets/icons/sidebar_inventory.svg";
 import SettingsIcon from "@assets/icons/sidebar_setting.svg";
 import LogoutIcon from "@assets/icons/sidebar_logout.svg";
 import { useState } from "react";
+import Chatbot from "@/components/chatbot/Chatbot";
 
 const sidebarLinks = [
-    { name: "Home", href: "/home", icon: DashboardIcon },
-    { name: "Catalog", href: "/catalog", icon: CatalogIcon },
-    { name: "Shelves", href: "/shelves", icon: ShelvesIcon },
-    { name: "My Reads", href: "/my-reads", icon: SavedIcon },
-    { name: "Profile", href: "/profile", icon: ProfileIcon },
+  { name: "Home", href: "/home", icon: DashboardIcon },
+  { name: "Catalog", href: "/catalog", icon: CatalogIcon },
+  { name: "Shelves", href: "/shelves", icon: ShelvesIcon },
+  { name: "My Reads", href: "/my-reads", icon: SavedIcon },
+  { name: "Profile", href: "/profile", icon: ProfileIcon },
 ];
 
 const sidebarFooterLinks = [
-    { name: "Settings", href: "/settings", icon: SettingsIcon },
-    { name: "Logout", href: "/login", icon: LogoutIcon },
+  { name: "Settings", href: "/settings", icon: SettingsIcon },
+  { name: "Logout", href: "/login", icon: LogoutIcon },
 ];
 
-function SidebarLink({ link, selectedLink, setSelectedLink }: { link: { name: string; href: string; icon: string }; selectedLink: string; setSelectedLink: (linkName: string) => void }) {
-    return (
-        <Link to={link.href} className='w-full' key={link.name} onClick={() => setSelectedLink(link.name)}>
-            <li key={link.name} className={`${selectedLink === link.name ? "bg-tertiary-container text-secondary" : "text-tertiary hover:bg-surface-container duration-200"}`}>
-                <img src={link.icon} alt={`${link.name} Icon`} className="sidebar-icon" />
-                <p>{link.name}</p>
-            </li>
-        </Link>
-    )
+function SidebarLink({
+  link,
+  selectedLink,
+  setSelectedLink,
+}: {
+  link: { name: string; href: string; icon: string };
+  selectedLink: string;
+  setSelectedLink: (linkName: string) => void;
+}) {
+  return (
+    <Link
+      to={link.href}
+      className="w-full"
+      key={link.name}
+      onClick={() => setSelectedLink(link.name)}
+    >
+      <li
+        key={link.name}
+        className={`${selectedLink === link.name ? "bg-tertiary-container text-secondary" : "text-tertiary hover:bg-surface-container duration-200"}`}
+      >
+        <img
+          src={link.icon}
+          alt={`${link.name} Icon`}
+          className="sidebar-icon"
+        />
+        <p>{link.name}</p>
+      </li>
+    </Link>
+  );
 }
 
 export default function Layout() {
-    const [selectedLink, setSelectedLink] = useState<string>("Home");
+  const [open, setOpen] = useState(false);
+  function handleChatbotComponent() {
+    setOpen((prev) => !prev);
+  }
+  const [selectedLink, setSelectedLink] = useState<string>("Home");
 
-    return (
-        <div className="layout">
-            <aside className="sidebar">
-                <h2 className="sidebar-title">Library Management</h2>
-                <nav className="sidebar-nav">
-                    <ul>
-                        {sidebarLinks.map((link) => (
-                            <SidebarLink
-                                key={link.name}
-                                link={link}
-                                selectedLink={selectedLink}
-                                setSelectedLink={setSelectedLink}
-                            />
-                        ))}
-                    </ul>
-                    <button className="ask-ai-button">
-                        <img src={DashboardIcon} alt="Ask AI Icon" className="ask-ai-icon" />
-                        <p>Ask AI</p>
-                    </button>
-                </nav>
-                <nav className="sidebar-footer">
-                    <ul>
-                        {sidebarFooterLinks.map((link) => (
-                            <SidebarLink
-                                key={link.name}
-                                link={link}
-                                selectedLink={selectedLink}
-                                setSelectedLink={setSelectedLink}
-                            />
-                        ))}
-                    </ul>
-                </nav>
-            </aside>
-            <Outlet />
-        </div>
-    );
+  return (
+    <div className="layout">
+      <aside className="sidebar">
+        <h2 className="sidebar-title">Library Management</h2>
+        <nav className="sidebar-nav">
+          <ul>
+            {sidebarLinks.map((link) => (
+              <SidebarLink
+                key={link.name}
+                link={link}
+                selectedLink={selectedLink}
+                setSelectedLink={setSelectedLink}
+              />
+            ))}
+          </ul>
+          <button className="ask-ai-button" onClick={handleChatbotComponent}>
+            <img
+              src={DashboardIcon}
+              alt="Ask AI Icon"
+              className="ask-ai-icon"
+            />
+            <p>Ask AI</p>
+          </button>
+        </nav>
+        <nav className="sidebar-footer">
+          <ul>
+            {sidebarFooterLinks.map((link) => (
+              <SidebarLink
+                key={link.name}
+                link={link}
+                selectedLink={selectedLink}
+                setSelectedLink={setSelectedLink}
+              />
+            ))}
+          </ul>
+        </nav>
+      </aside>
+      {open && <Chatbot isBotOpen={handleChatbotComponent} />}
+      <Outlet />
+    </div>
+  );
 }
