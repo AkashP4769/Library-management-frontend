47365fba6a3a7aa1579758ab67a8bc9615b7eeef
Aksa <aksageorge2703@gmail.com>
2026-06-30 11:48:49 +0530

    fix: remove commented lines

--- src/pages/employee/MyReads.tsx
+++ src/pages/employee/MyReads.tsx
@@ -4,33 +4,6 @@ import { books } from "@/models/book";
 import { FaUser, FaEnvelope, FaPhoneAlt } from "react-icons/fa";
 import EmptyShelf from "@/Components/EmptyShelf";
 
-// export default function MyReads() {
-//     return (
-// <div className="my-reads-page">
-//     <section className="my-reads-section">
-//         <h1 className="text-4xl font-bold mb-4">My Reads</h1>
-//         <p className="text-lg text-gray-600">This is the My Reads page.</p>
-//         <p className="text-lg text-gray-600">You can add more details about your reads here.</p>
-//     </section>
-
-//     {/* borrowed books section */}
-//     <section className="borrowed-books-section">
-//         <h2 className="text-2xl font-bold">Borrowed Books</h2>
-//         <p className="text-lg text-gray-600">These are the books you have borrowed.</p>
-//     </section>
-
-//     {/* Books requested section */}
-//     <section className="books-requested-section">
-//         <h2 className="text-2xl font-bold">Books Requested</h2>
-//         <p className="text-lg text-gray-600">These are the books you have requested.</p>
-//     </section>
-
-//     {/* saved books section */}
-//     <section className="saved-books-section">
-//         <h2 className="text-2xl font-bold">Saved Books</h2>
-//         <p className="text-lg text-gray-600">These are the books you have saved for later.</p>
-//     </section>
-// </div>
 export default function MyReads() {
   const borrowedBooks = books.slice(0, 2);
   const requestedBooks = books.slice(2, 3);
@@ -45,7 +18,6 @@ export default function MyReads() {
             <div className="w-24 h-24 rounded-full bg-primary-container text-white flex items-center justify-center text-3xl font-bold">
               <FaUser />
             </div>
-
             <div>
               <h2 className="text-2xl font-bold">Aksa George</h2>
               <p className="text-primary font-medium">Employee</p>
