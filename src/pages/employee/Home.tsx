3409a594da725cf4a6de9fb81fcc8bde5727d975
Akash <awesomeakash47@gmail.com>
2026-06-30 16:34:06 +0530

    fix: image preview issue fixed

--- src/pages/employee/Home.tsx
+++ src/pages/employee/Home.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
 
     useEffect(() => {
         if (fetchedBooks) {
-            setMyBooks(fetchedBooks);
+            setMyBooks([...fetchedBooks, ...fetchedBooks]);
         }
     }, [fetchedBooks]);
 
